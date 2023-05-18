88dc35a7c4a4f2a66a26a650c2ea0a7cba63243d
enamoredevil <enamoredevil@gmail.com>
2023-05-18 16:29:50 +0400

    hero_component_added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "../header/Header";
+import Hero from "../hero/Hero";
 
 const App = () => {
   return (
     <div className="app">
         <Header />
+        <Hero />
     </div>
   );
 };
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import previewSneakers from "../../assets/preview-sneakers.webp";
+
+import "./hero.scss";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__container container">
+        <div className="hero__info">
+          <h1 className="hero__title">
+            NIKE WMNS AIR MAX 97 SE <br />
+            already in stock
+          </h1>
+          <p className="hero__description">
+            Check out our collection of over 50 Nike models
+          </p>
+          <a className="hero__link" href="/">Catalog</a>
+        </div>
+        <div className="hero__image-block">
+          <img src={previewSneakers} alt="nike-wmns-air-max-97-se" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
