0cb797baad26d767cf6d6e17ea57fefad77ce242
enamoredevil <enamoredevil@gmail.com>
2023-05-18 18:23:55 +0400

    footer_component_added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,16 +3,17 @@ import React from "react";
 import Header from "../header/Header";
 import Hero from "../hero/Hero";
 import Catalog from "../catalog/Catalog";
+import Footer from "../footer/Footer";
 
 const App = () => {
   return (
     <div className="app">
-        <Header />
-        <Hero />
-        <Catalog />
+      <Header />
+      <Hero />
+      <Catalog />
+      <Footer />
     </div>
   );
 };
-
 
 export default App;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import "./footer.scss";
+
+import visa from "../../assets/visa.png";
+import mastercard from "../../assets/mastercard.png";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <div className="footer__description">
+          <h4>Shoes</h4>
+          <p>
+            Do you want to buy any shoes? You've come to the right place! Shoes
+            is an online store that has a huge selection of popular sneakers. A
+            wide range of models of popular brands from new collections will
+            please many sneakerheads. Our online store sells shoes for men,
+            women and all the models fit the size grid, so you can easily buy
+            new sneakers.
+          </p>
+          <span>© 2023, Shoes.com</span>
+        </div>
+        <div className="footer__info">
+          <div className="footer__payments">
+            <h4>We accept for payments:</h4>
+            <div>
+              <img src={visa} alt="visa" />
+              <img src={mastercard} alt="mastercard" />
+            </div>
+          </div>
+          <div className="footer__services">
+            <div>
+              <h4>Working hours:</h4>
+              <span>Mon.-Fri.: 9 am - 9 pm</span>
+            </div>
+            <div>
+              <h4>Email:</h4>
+              <span>Shoes@gmail.com</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
